39bb1d1945bb80095b7d23132e0e63fdeb1307a5
tatuan0312 <85239207+tatvlogs0312@users.noreply.github.com>
2022-09-29 15:26:42 +0700

    update

---
+++ QLSP/demo/src/main/resources/category_202209291525.sql
@@ -0,0 +1,7 @@
+INSERT INTO QLSP.category (category_id,category) VALUES
+	 ('1','Văn phòng phẩm'),
+	 ('2','Thực phẩm'),
+	 ('29a29755-80bf-4d46-a559-f4a999771693','Dầu gội'),
+	 ('3635cc1c-7372-4e1c-ab06-951cfcaf4ed6','Thời trang'),
+	 ('73d4b8a4-1160-41ae-b5c5-b16ec2ca0e4f','Gia vị'),
+	 ('9837be89-fd3e-441e-a756-386f4e09e9b9','Khẩu trang');
---
+++ QLSP/demo/src/main/resources/product_202209291526.sql
@@ -0,0 +1,26 @@
+INSERT INTO QLSP.product (id_product,description,name,category_id) VALUES
+	 ('184f9aed-bf83-4b12-b149-1206c184c099','0wafafa
+','11','1'),
+	 ('1d063c26-1cf7-4755-b852-5a98ef759b37','111000','0010','29a29755-80bf-4d46-a559-f4a999771693'),
+	 ('2','trắng','tẩy 3k','1'),
+	 ('2daad462-5f88-4ddc-aefd-50d86f5ffbcb','ávsav','accc','2'),
+	 ('3','Mì tôm hảo hảo','Mì tôm','2'),
+	 ('34460882-9712-4316-a687-98bbb8de9e9b','asdzsxgchbj,','sàdfcgnvbmj','1'),
+	 ('3ec5f8e5-daeb-477b-829e-fdf065291c34','sadfdsgdhv','dasfdghvmb,n','1'),
+	 ('3f942300-717c-4422-911f-6ba3cadf5951','ưqertyui','fegrhtd','9837be89-fd3e-441e-a756-386f4e09e9b9'),
+	 ('4fa7682e-c77c-48e9-9f5e-806e9a639158','sgsdfdm,n','âcsv','2'),
+	 ('69b30d17-f215-40bb-b94d-c217461a0c31','aaa','aa','1');
+INSERT INTO QLSP.product (id_product,description,name,category_id) VALUES
+	 ('6e48f3d2-a23c-4d20-82de-10fd9ef0d054','dfghjkl;','asdfgjl;''','3635cc1c-7372-4e1c-ab06-951cfcaf4ed6'),
+	 ('73583382-cbbc-4464-97d7-871726eb1c8e','1','1','1'),
+	 ('756e3a9f-d2fe-40d4-b84c-6b149068c915','qw','qw','1'),
+	 ('7baaff70-4d1f-4beb-b42d-87ceaa7dc610','faesgh','SFD','1'),
+	 ('7c3643a0-abdf-48ed-a25c-d932bcec58fb','dửaest','ưqreo','3635cc1c-7372-4e1c-ab06-951cfcaf4ed6'),
+	 ('8037f1a3-d699-44ec-b34e-97ab3615f877','dtufyiguohjo[kp','gfhjjkpl[;','1'),
+	 ('ab80c5d2-8dfc-4fe6-b24d-a071eaacac22','zcvdsghtjy','sàdgsfhgjh','1'),
+	 ('cb097fb2-2936-4eae-b26f-7cad29740ba9','aa','a','1'),
+	 ('d2b8e660-ec2e-44e2-bde0-6b1e573bba82','khẩu trang đen 4 lớp','khẩu trang đen 4 lớp','9837be89-fd3e-441e-a756-386f4e09e9b9'),
+	 ('eb176007-a563-4150-8b93-0119a4527d06','ưery','sá.kbdg','3635cc1c-7372-4e1c-ab06-951cfcaf4ed6');
+INSERT INTO QLSP.product (id_product,description,name,category_id) VALUES
+	 ('f0cbe5cd-d51c-4180-81ae-f4613b2358f3','Phở ăn liền','Phở ','2'),
+	 ('fbccdc0c-8df3-4861-aaad-4b6817bfe778','00002353','0001','1');
